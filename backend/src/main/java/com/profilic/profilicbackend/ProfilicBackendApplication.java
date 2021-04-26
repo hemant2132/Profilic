91a9e77083faeb0bfe37dfa6df086c5653ba78f7
Hemant <hemant2132@gmail.com>
2021-04-26 20:39:24 +0530

    Initial commit

---
+++ backend/src/main/java/com/profilic/profilicbackend/ProfilicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.profilic.profilicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilicBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/profilic/profilicbackend/ProfilicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profilic.profilicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
